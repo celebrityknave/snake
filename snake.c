38a9b0d21ac1cf07ee7ecb4f1ca497b686c413f5
Evan Blake <evan.peter.blake@gmail.com>
2021-02-13 15:35:42 +1100

    implemented logic for snake embiggening

--- snake.c
+++ snake.c
@@ -9,6 +9,10 @@
 #define INITIAL_WINDOW_WIDTH 500
 #define INITIAL_WINDOW_HEIGHT 500
 
+// TODO: make snake have length.
+//		Efficient method: prevent gl from wiping square drawing for snake.length timer ticks
+//		Expensive method: hold all x and y co-ordinates for snake.
+
 short snake_direction = 0;
 const int target_fps = 4;
 //const float time_per_frame = 1000 / target_fps;
@@ -36,8 +40,12 @@ GLfloat rand_low()
 struct {
 	GLsizei window_size[2];
 	GLfloat size;
-	GLfloat x;
-	GLfloat y;
+	struct {
+		GLfloat x;
+		GLfloat y;
+	} head;
+	GLfloat x[256];
+	GLfloat y[256];
 	GLfloat direction;
 	GLint length;
 } snake;
@@ -76,8 +84,12 @@ void display() {  // Display function will draw the image.
     glClearColor( 0, 0, 0, 1 );  // (In fact, this is the default.)
     glClear( GL_COLOR_BUFFER_BIT );
 
-	draw_square(snake.x, snake.y, snake.size);
+	draw_square(snake.head.x, snake.head.y, snake.size);
 	draw_square(food.x, food.y, food.size);
+	for(int i=0; i<snake.length; ++i)
+	{
+		draw_square(snake.x[i], snake.y[i], snake.size);
+	}
 
     glutSwapBuffers(); // Required to copy color buffer onto the screen.
 }
@@ -90,38 +102,34 @@ static void keyboard(unsigned char key, int x, int y)
 	}
 	if(key == 'w' || key == 'W')
 	{
-		//snake_y += 0.1;
 		if(snake_direction != 1 && snake_direction != 0)
 		{
 			snake_direction = 0;
-			fprintf(stdout, "Up, y=%f\n", snake.y);
+			fprintf(stdout, "Up, y=%f\n", snake.head.y);
 		}
 	}
 	if(key == 's' || key == 'S')
 	{
-		//snake_y -= 0.1;
 		if(snake_direction != 0 && snake_direction != 1)
 		{
 			snake_direction = 1;
-			fprintf(stdout, "Down, y=%f\n", snake.y);
+			fprintf(stdout, "Down, y=%f\n", snake.head.y);
 		}
 	}
 	if(key == 'a' || key == 'A')
 	{
-		//snake_x -= 0.1;
 		if(snake_direction != 3 && snake_direction != 2)
 		{
 			snake_direction = 2;
-			fprintf(stdout, "Left, x=%f\n", snake.x);
+			fprintf(stdout, "Left, x=%f\n", snake.head.x);
 		}
 	}
 	if(key == 'd' || key == 'D')
 	{
-		//snake_x += 0.1;
 		if(snake_direction != 2 && snake_direction != 3)
 		{
 			snake_direction = 3;
-			fprintf(stdout, "Right, x=%f\n", snake.x);
+			fprintf(stdout, "Right, x=%f\n", snake.head.x);
 		}
 	}
 }
@@ -142,21 +150,43 @@ static void reshape(int w, int h)
 }
 static void timer (int msec)
 {
-	fprintf(stdout, "Snake x: %f, food x: %f, snake y: %f, food y: %f\n", snake.x, food.x, snake.y, food.y);
-	if(fequal(snake.x, food.x) && fequal(snake.y, food.y))
+	if(fequal(snake.head.x, food.x) && fequal(snake.head.y, food.y))
+	{
+		fprintf(stdout, "Snake x: %f, snake y: %f,  snake length: %d\n", snake.head.x, snake.head.y, snake.length);
+
+		snake.x[snake.length-1] = snake.head.x;
+		snake.y[snake.length-1] = snake.head.y;
+		snake.length++;
 		gen_food();
+	}
+
+	for(int i=snake.length; i >= 0; --i)
+	{
+		if(i == 0)
+		{
+			snake.x[0] = snake.head.x;
+			snake.y[0] = snake.head.y;
+		}
+		else
+		{
+			snake.x[i] = snake.x[i-1];
+			snake.y[i] = snake.y[i-1];
+		}
+		fprintf(stdout, "Co-ordinates of x[%d]: %f, %f\n", i, snake.x[i], snake.y[i]);
+	}
+
 	switch(snake_direction) {
 		case 0:
-			snake.y += 0.1f;
+			snake.head.y += 0.1f;
 			break;
 		case 1:
-			snake.y -= 0.1f;
+			snake.head.y -= 0.1f;
 			break;
 		case 2:
-			snake.x -= 0.1f;
+			snake.head.x -= 0.1f;
 			break;
 		case 3:
-			snake.x += 0.1f;
+			snake.head.x += 0.1f;
 			break;
 	}
 	glutTimerFunc(sleep_t, timer, 0);
@@ -172,8 +202,9 @@ int main( int argc, char** argv )
 	snake.window_size[0] = INITIAL_WINDOW_WIDTH;
 	snake.window_size[1] = INITIAL_WINDOW_HEIGHT;
 	snake.size = 0.05;
-	snake.x = 0;
-	snake.y = 0;
+	snake.head.x = 0;
+	snake.head.y = 0;
+	snake.length = 1;
 	food.size = 0.02;
 
     glutInit(&argc, argv);				 // Initialize GLUT and
@@ -192,4 +223,3 @@ int main( int argc, char** argv )
     return 0;
 
 }
-
