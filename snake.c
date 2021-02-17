cf56ab2467666d461de72a201c36ac5633a70ef8
Evan Blake <evan.peter.blake@gmail.com>
2021-02-17 14:55:03 +1100

    display now drawing from grid co-ordinates

--- snake.c
+++ snake.c
@@ -19,9 +19,7 @@ const float grid_increment = 1.0f / grid_size;
 
 // For comparing float values
 float epsilon = 0.00001;
-
-int x_position;
-int y_position;
+//bool** grid;
 
 int msleep(unsigned int tms) {
 	return usleep(tms * 1000);
@@ -31,6 +29,8 @@ int msleep(unsigned int tms) {
 bool fequal(GLfloat a, GLfloat b) {
 	return fabs(a-b) < epsilon;
 }
+
+bool grid[64][64];
 
 // Generate random number between -1.0 and 1.0 in 1/grid_size increments
 GLfloat rand_low() {
@@ -43,10 +43,8 @@ return (rand() % grid_size * 2);
 struct {
 	GLsizei window_size[2];
 	GLfloat size;
-	struct {
-		int x;
-		int y;
-	} head;
+	int x_position;
+	int y_position;
 	int x[256];
 	int y[256];
 	GLint direction;
@@ -54,8 +52,6 @@ struct {
 } snake;
 
 struct {
-	GLfloat x;
-	GLfloat y;
 	int x_position;
 	int y_position;
 	GLfloat size;
@@ -71,7 +67,7 @@ void draw_square(int grid_x, int grid_y, GLfloat square_size)
 	if(grid_y - grid_size == 0)
 		y = 0.0f;
 
-	fprintf(stdout, "position: %f, %f\n", x, y);
+	//fprintf(stdout, "position: %f, %f\n", x, y);
 
     glBegin(GL_POLYGON);
     glColor3f( 0, 1, 0 );
@@ -93,6 +89,7 @@ void gen_food()
 	food.x_position = rand_grid();
 	food.y_position = rand_grid();
 
+	// TODO remove this loop and write new function that checks unfilled contents of grid[]
 	for(int i=0; i < snake.length; ++i)
 	{
 		if( food.x_position == snake.x[i] )
@@ -113,14 +110,16 @@ void display() {  // Display function will draw the image.
     glClearColor( 0, 0, 0, 1 );  // (In fact, this is the default.)
     glClear( GL_COLOR_BUFFER_BIT );
 
-	draw_square(x_position, y_position, snake.size);
+	//draw_square(snake.x_position, snake.y_position, snake.size);
 	draw_square(food.x_position, food.y_position, food.size);
 
-
-	// redo this with new co-ordinate system
-	for(int i=0; i<snake.length; ++i)
+	for(int i=0; i < grid_size*2; ++i)
 	{
-		draw_square(snake.x[i], snake.y[i], snake.size);
+		for(int j=0; j < grid_size*2; ++j)
+		{
+			if(grid[i][j])
+				draw_square(i, j, snake.size);
+		}
 	}
 
     glutSwapBuffers(); // Required to copy color buffer onto the screen.
@@ -137,7 +136,7 @@ static void keyboard(unsigned char key, int x, int y)
 		if(snake.direction != 1 && snake.direction != 0)
 		{
 			snake.direction = 0;
-			fprintf(stdout, "Up, y=%d\n", y_position);
+			fprintf(stdout, "Up, y=%d\n", snake.y_position);
 		}
 	}
 	if(key == 's' || key == 'S')
@@ -145,7 +144,7 @@ static void keyboard(unsigned char key, int x, int y)
 		if(snake.direction != 0 && snake.direction != 1)
 		{
 			snake.direction = 1;
-			fprintf(stdout, "Down, y=%d\n", y_position);
+			fprintf(stdout, "Down, y=%d\n", snake.y_position);
 		}
 	}
 	if(key == 'a' || key == 'A')
@@ -153,7 +152,7 @@ static void keyboard(unsigned char key, int x, int y)
 		if(snake.direction != 3 && snake.direction != 2)
 		{
 			snake.direction = 2;
-			fprintf(stdout, "Left, x=%d\n", x_position);
+			fprintf(stdout, "Left, x=%d\n", snake.x_position);
 		}
 	}
 	if(key == 'd' || key == 'D')
@@ -161,7 +160,7 @@ static void keyboard(unsigned char key, int x, int y)
 		if(snake.direction != 2 && snake.direction != 3)
 		{
 			snake.direction = 3;
-			fprintf(stdout, "Right, x=%d\n", x_position);
+			fprintf(stdout, "Right, x=%d\n", snake.x_position);
 		}
 	}
 }
@@ -183,25 +182,39 @@ static void reshape(int w, int h)
 }
 static void timer(int msec)
 {
-	if(x_position == food.x_position && y_position == food.y_position)
+	if(snake.x_position == food.x_position && snake.y_position == food.y_position)
 	{
-		snake.x[snake.length-1] = x_position;
-		snake.y[snake.length-1] = y_position;
+		snake.x[snake.length-1] = snake.x_position;
+		snake.y[snake.length-1] = snake.y_position;
 		snake.length++;
 		gen_food();
 
-		fprintf(stdout, "Snake x: %d, snake y: %d,  snake length: %d\n", x_position, y_position, snake.length);
+		fprintf(stdout, "Snake x: %d, snake y: %d,  snake length: %d\n", snake.x_position, snake.y_position, snake.length);
 	}
-	//fprintf(stdout, "Food location: %d, %d\n", food.x_position, food.y_position);
 
-	// This is really expensive. Should replace it with a 'hold snake.head coords for
+	switch(snake.direction) {
+		case 0:
+			snake.y_position++;
+			break;
+		case 1:
+			snake.y_position--;
+			break;
+		case 2:
+			snake.x_position--;
+			break;
+		case 3:
+			snake.x_position++;
+			break;
+	}
+
+	// This is really expensive. Should replace it with a 'hold position coords for
 	// snake.length steps' setup
 	for(int i=snake.length; i >= 0; --i)
 	{
 		if(i == 0)
 		{
-			snake.x[0] = x_position;
-			snake.y[0] = y_position;
+			snake.x[0] = snake.x_position;
+			snake.y[0] = snake.y_position;
 		}
 		else
 		{
@@ -210,25 +223,19 @@ static void timer(int msec)
 		}
 	}
 
-	switch(snake.direction) {
-		case 0:
-			snake.head.y += grid_increment;
-			y_position++;
-			break;
-		case 1:
-			snake.head.y -= grid_increment;
-			y_position--;
-			break;
-		case 2:
-			snake.head.x -= grid_increment;
-			x_position--;
-			break;
-		case 3:
-			snake.head.x += grid_increment;
-			x_position++;
-			break;
+	// This could be more efficient
+	for(int i=0; i < grid_size*2; ++i)
+	{
+		for(int j=0; j < grid_size*2; ++j)
+		{
+			grid[i][j] = 0;
+		}
 	}
-	//grid[x_position][y_position] = 1;
+
+	for(int i=0; i < snake.length; ++i)
+	{
+		grid[snake.x[i]][snake.y[i]] = 1;
+	}
 
 	glutPostRedisplay();
 	glutTimerFunc(sleep_t, timer, 0);
@@ -241,7 +248,10 @@ static void update(void)
 
 int main( int argc, char** argv )
 {
-    bool grid[grid_size*2][grid_size*2];
+	printf("test");
+
+	//bool ** grid = (bool**)malloc(grid_size*2 * sizeof(bool));
+
 	for(int i=0; i < grid_size*2; ++i)
 	{
 		for(int j=0; j < grid_size*2; ++j)
@@ -251,19 +261,17 @@ int main( int argc, char** argv )
 	}
 
 	// The snake starts in the middle of the arena, therefore:
-	x_position = grid_size;
-	y_position = grid_size;
+	snake.x_position = grid_size;
+	snake.y_position = grid_size;
 
 	food.x_position = rand_grid();
 	food.y_position = rand_grid();
 
-	grid[x_position][y_position] = 1;
+	//grid[snake.x_position][snake.y_position] = 1;
 	snake.window_size[0] = INITIAL_WINDOW_WIDTH;
 	snake.window_size[1] = INITIAL_WINDOW_HEIGHT;
 	snake.size = grid_increment / 2;
-	snake.head.x = 0;
-	snake.head.y = 0;
-	snake.length = 0;
+	snake.length = 1;
 	snake.direction = 0;
 	food.size = grid_increment / 5;
 
@@ -276,10 +284,10 @@ int main( int argc, char** argv )
 	glutTimerFunc(sleep_t, timer, 0);
 	//glutReshapeFunc(&reshape);
     glutKeyboardFunc(&keyboard);
-	//glutIdleFunc(&update);
 
     glutMainLoop(); // Run the event loop!  This function does not return.
                     // Program ends when user closes the window.
-    return 0;
 
+	//free(grid);
+    return 0;
 }
